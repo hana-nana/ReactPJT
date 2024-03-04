927f5b5e5d6fcaf21bb43ab87655da06920decff
Hana Na <gksk3185@gmail.com>
2024-03-04 10:30:46 +0900

    README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ReactPJT
-123131311
+1) movie information list pjt
+- 영화 목록(제목, 포스터, 내용 요약, 장르, 연도) 제공
+
+2) todo list pjt
+- todo list 목록 만들기
+
+3) coin tracker pjt
+- 코인 목록(2000개) 소개
+
+4) unit converter pjt
+- 시간, 분 단위 변환기
--- todo-list-pjt/src/App.js
+++ todo-list-pjt/src/App.js
@@ -1,48 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    // input창이 비어있을 경우, 그냥 return해서 onSubmit함수가 작동하지 않도록
-    if(toDo === "") {
-      return;
-    }
-    // <array를 직접적으로 수정하지 않으면서 setToDos로 array에 element를 추가하는 방법>
-    // 1) 값을 직접 수정 : toDo를 추가, 그리고 input 비우기
-    setToDo("");
-    // 2) 함수를 넣기 : 첫번째 인자로 현재 state받아오고, 누적해서 새로운 toDo붙인 새로운 배열 받아오기
-    setToDos((currentArray) => [toDo, ...currentArray]);
-  };
+  // 로딩을 위한 state
+  const [loading, setLoading] = useState(true);
+  // 코인 리스트를 App 컴포넌트에서 보여주기 위한 새로운 State 생성
+  const [coins, setCoins] = useState([])
+  
+  // useEffect에 빈배열을 넣으면 아래 코드는 한번만 작동
+  // json = coins
+  // coins얻기를 끝냈다면 loading을 false로 바꿈
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then(response => response.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);  
+    });
+  }, [])
 
-  return (
+    // 만약 로딩 중이라면 Loading...메시지를 보여주고, 그렇지 않으면 아무것도 안보여줌
+    // coin이라는 변수는 coins라는 배열안에 있는 각각의 coin을 의미
+    // coins라는 변수에 코인들의 배열이 담겨있으므로 map()함수를 사용해서 각 코인들을 보여줄 것
+    // map함수는 첫번째 인자로 데이터, 두번째 인자로 index(key)를 받는데, 이미 json 데이터 구성 상 id값이 존재하므로 index를 안받음. id값을 key로 사용
+    return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-        onChange={onChange} 
-        value={toDo} 
-        type="text" 
-        placeholder="Write your to do..." 
-        
-        />
-        <button>Add To Do</button>
-      </form>
-
-      <hr />
-      {/* todo list의 모든 내용이 한 배열에 다 들어가 있었으므로
-      1) toDos 배열을 가져와서 
-      2) 그 배열의 item들이 각각의 li태그가 되도록 하기위해 map함수 사용
-      - map함수 : 기존 배열 내부 요소에 각각 접근해서 함수 내부명령 실행하고 새로운 배열로 반환, 첫번째 인자는 value, 두번째 인자는 index 
-      - 아래에서 map함수의 용도 : 함수 안에 item을 넣어서 그냥 각 item자체를 return*/}
+      <h1>The Coins! { loading ? "" : `(${coins.length})` }</h1>
+      {loading ? <strong>Loading...</strong> : null }
       <ul>
-        {toDos.map((item, index) => (
-        <li key={index}>{item}</li>
+        {coins.map((coin) => (
+          <li>
+            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+          </li>
         ))}
       </ul>
     </div>
   );
-};
-
+  };
 export default App;
